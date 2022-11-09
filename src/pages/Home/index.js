bf93cf25a6fefd22a3718f322acf209dde125485
Shreyansh Agarwal <shreyansh@aus.co.in>
2022-11-09 16:57:28 +0530

    Add Button component file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,11 @@ import './home.css';
 export const Home = () => {
   return (
     <div className="home">
-      <header className="home-header">Header</header>
+      <header className="home-header">
+        {/* Header Section */}
+        {/* Club Section */}
+        {/* Footer Section */}
+      </header>
     </div>
   );
 };
