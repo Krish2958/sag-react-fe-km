43ddf9490f58ca1e63760ced8c80a14995ed0ee6
Shreyansh Agarwal <shreyansh@aus.co.in>
2022-11-10 22:57:26 +0530

    Update css import order

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './pages';
 import './app.css';
+import { Home } from './pages';
 
 const App = () => {
   return (
