27253fdeba13ac216ae65ed8b26fa851299daa6b
Shreyansh Agarwal <shreyansh@aus.co.in>
2022-12-08 23:29:34 +0530

    Order exports alphabetically

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 export { default as Button } from './Button';
-
-export { default as Pill } from './Pill';
-
 export { default as Icon, IconIdentifier } from './Icon';
 export { default as PhotoCarousel, PhotoCarouselItem } from './PhotoCarousel';
+export { default as Pill } from './Pill';
