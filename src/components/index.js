5b97fd755ce900deb337870f8f21ff94b7cabc2d
Shreyansh Agarwal <shreyansh@aus.co.in>
2022-12-08 23:27:39 +0530

    [SWA-49] Add photo carousel component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from './Button';
 export { default as Icon, IconIdentifier } from './Icon';
+export { default as PhotoCarousel, PhotoCarouselItem } from './PhotoCarousel';
