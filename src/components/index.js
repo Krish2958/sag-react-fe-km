b6a2f8c72f65398ff2bc3978b52958dcd4abad7c
Shreyansh Agarwal <shreyansh@aus.co.in>
2022-12-13 22:45:27 +0530

    Add all club logos in frontend repo

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Button, ButtonVariant } from './Button';
-export { default as Logo } from './Logo';
+export { default as Logo, LogoIdentifier } from './Logo';
 export { default as Icon, IconIdentifier } from './Icon';
 export { default as PhotoCarousel, PhotoCarouselItem } from './PhotoCarousel';
 export { default as Pill } from './Pill';
