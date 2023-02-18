b45a716d14784d65686e2933a25614ee0f9f8a95
Krish2958 <krishm.km17@gmail.com>
2023-02-19 00:48:59 +0530

    Added OAuth 2 Setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 // Add bootstrap to the project
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
@@ -9,7 +9,10 @@ import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId="987099510004-2nqd8vmmoo7vnpiuegldrp8hs2phkheq.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
+  document.getElementById('root'),
 );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { EventsSection } from './components';
 import './home.css';
+import { GoogleLogin } from '@react-oauth/google';
 
+const responseMessage = (response) => {
+  console.log(response);
+};
+const errorMessage = (error) => {
+  console.log(error);
+};
 export const Home = () => {
   return (
     <div className="home">
       {/* Header Section */}
       <header>SAG Web App started successfully!</header>
-
+      <div>
+        <h4>React Google Login</h4>
+        <br />
+        <br />
+        <div>
+          <GoogleLogin
+            useOneTap="True"
+            onSuccess={responseMessage}
+            onError={errorMessage}
+          />
+        </div>
+      </div>
       {/* Upcoming Events Section */}
-      <EventsSection />
 
       {/* Club Section */}
       {/* Footer Section */}
