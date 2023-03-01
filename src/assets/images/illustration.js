2458df4c6d4381f2b358a3020dd5668e41ffe1dd
Shreyansh Agarwal <shreyansh@aus.co.in>
2023-03-01 09:43:24 +0530

    hotfix: move illustration handling

--- src/assets/images/illustration.js
+++
@@ -1,7 +0,0 @@
-import UserInDoubt from './illustrations/user-in-doubt.svg';
-import UserChilling from './illustrations/user-chilling.svg';
-
-export const Illustration = {
-  UserInDoubt,
-  UserChilling,
-};
