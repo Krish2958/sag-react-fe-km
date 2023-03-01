814a541b349260a917aa213be3adee55d075b4db
Shreyansh Agarwal <shreyansh@aus.co.in>
2023-03-01 09:11:26 +0530

    Move logo identifiers to /assets/images/ folder

--- src/components/Logo/LogoIdentifier.js
+++ src/components/Logo/LogoIdentifier.js
@@ -1,11 +1,11 @@
-import ArtAndCraftClubLogo from '../../assets/club-logos/art-n-craft-club-logo.png';
-import CulturalClubLogo from '../../assets/club-logos/cultural-club-logo.png';
-import HumanValuesClubLogo from '../../assets/club-logos/human-values-club-logo.png';
-import JournalismClubLogo from '../../assets/club-logos/journalism-club-logo.png';
-import LiteralClubLogo from '../../assets/club-logos/literal-club-logo.png';
-import ManagementClubLogo from '../../assets/club-logos/management-club-logo.png';
-import MathClubLogo from '../../assets/club-logos/math-club-logo.png';
-import PublicationClubLogo from '../../assets/club-logos/publication-club-logo.png';
+import ArtAndCraftClubLogo from '../../assets/images/club-logos/art-n-craft-club-logo.png';
+import CulturalClubLogo from '../../assets/images/club-logos/cultural-club-logo.png';
+import HumanValuesClubLogo from '../../assets/images/club-logos/human-values-club-logo.png';
+import JournalismClubLogo from '../../assets/images/club-logos/journalism-club-logo.png';
+import LiteralClubLogo from '../../assets/images/club-logos/literal-club-logo.png';
+import ManagementClubLogo from '../../assets/images/club-logos/management-club-logo.png';
+import MathClubLogo from '../../assets/images/club-logos/math-club-logo.png';
+import PublicationClubLogo from '../../assets/images/club-logos/publication-club-logo.png';
 
 export const LogoIdentifier = {
   ArtAndCraftClub: ArtAndCraftClubLogo,
