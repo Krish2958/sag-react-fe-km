afa2b980b98eb72c4945f91863d08ddcc9814313
Krish2958 <krishm.km17@gmail.com>
2023-03-05 15:24:27 +0530

    Added OAuth Consent Screen

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 // Add bootstrap to the project
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import './index.css';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId="#">
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </GoogleOAuthProvider>,
 );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Illustration } from '../../assets/images';
 import { Button, ButtonVariant, Icon, IconIdentifier } from '../../components';
 import './Login.css';
+import { useGoogleLogin } from '@react-oauth/google';
+
 
 export const Login = () => {
+  const signin = useGoogleLogin({
+    onSuccess: codeResponse => console.log(codeResponse),
+    // flow: 'auth-code',
+    hosted_domain: 'mlvti.ac.in',
+    onError:() => console.log('Login Failed'),
+  }
+  );
   return (
     <div className="login-page">
       <div className="login-form-container">
         <h1 className="title">Get Started with SAG</h1>
         <img className="illustration" src={Illustration.UserChilling} />
-        <Button className="login-form-button" variant={ButtonVariant.Secondary}>
+        <Button className="login-form-button" variant={ButtonVariant.Secondary} onClick={() => signin()}>
           <Icon iconIdentifier={IconIdentifier.Google} /> Sign-in with Google
         </Button>
       </div>
