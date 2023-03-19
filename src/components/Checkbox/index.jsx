40e0ab7aa1656cff8bfa7957596395588b97edac
Its-Devendra <devendrasinghshekhawat87@gmail.com>
2023-03-19 23:48:48 +0530

    SWA-149 [APP] Add Checkbox

---
+++ src/components/Checkbox/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import './Checkbox.css';
+const CheckBox = () => {
+  return (
+    <>
+      <Form.Check className="checks" aria-label="option 1" />
+    </>
+  );
+};
+
+export default CheckBox;
