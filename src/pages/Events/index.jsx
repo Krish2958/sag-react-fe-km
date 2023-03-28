a7ea7c37cd9135e980e75e961af0ab831ce5978d
Krish2958 <krishm.km17@gmail.com>
2023-03-28 15:03:47 +0530

    Removed test import

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { Badge, IconIdentifier } from '../../components';
 import { Colors } from '../../components';
 export const Events = () => {
-  return (
-    <div className="title">
-      Events
-      <Badge iconIdentifier={IconIdentifier.Bulb} color={Colors.PillBlue}>
-        Hello
-      </Badge>
-    </div>
-  );
+  return <div className="title">Events</div>;
 };
 
 Events.route = '/events';
