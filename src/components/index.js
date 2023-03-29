f225e7602d18f34215bf1827714ac56eef93452a
Krish2958 <krishm.km17@gmail.com>
2023-03-30 00:43:41 +0530

    Updated Pill Component to use Colors class instead of variants

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,4 @@ export { default as Button, ButtonVariant } from './Button';
 export { default as Logo, LogoIdentifier } from './Logo';
 export { default as Icon, IconIdentifier } from './Icon';
 export { default as PhotoCarousel, PhotoCarouselItem } from './PhotoCarousel';
-export { default as Pill, PillVariant } from './Pill';
+export { default as Pill } from './Pill';
