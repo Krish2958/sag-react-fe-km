aacabbc49ccbf4ce8abb05735636503f38b8d592
Krish2958 <krishm.km17@gmail.com>
2023-04-04 10:16:33 +0530

    old design removal

--- src/pages/Events/components/Card/index.jsx
+++ src/pages/Events/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import './EventCard.css';
+import { Icon, Logo, LogoIdentifier } from '..//..//..//..//components';
 const EventCard = () => {
   return (
     <Card className="ECard">
@@ -10,19 +11,11 @@ const EventCard = () => {
         src="src/assets/images/Rectangle 224.png"
       />
       <Card.Body>
-        <Card.Title >
-          <div className='bstitle'>
-          Essay Writing Competition</div>
-        <Card.Text>
-          <h5 className="club ">Publication Club</h5>
-        <div className="square">
-          <div className="content">
-          <h3 className="date">24</h3>
-          <h5 className="month">Mar</h5>
-          </div>
+        <Card.Title className="ititle">Essay Writing Competition</Card.Title>
+        <div className="club">
+          {/* <Icon imageSource='src/assets/images/club-logos/publication-club-logo.png' iconSize='2rem' ></Icon> */}
+          <Card.Text className="textclub">Publication Club</Card.Text>
         </div>
-        </Card.Text>
-        </Card.Title>
       </Card.Body>
     </Card>
   );
