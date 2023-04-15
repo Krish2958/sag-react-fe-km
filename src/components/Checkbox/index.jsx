395aa3bed1f219620fed23c086f52aecfcf9fb58
Its-Devendra <devendrasinghshekhawat87@gmail.com>
2023-04-15 17:10:45 +0530

    Added some changes

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { FormCheck } from 'react-bootstrap';
-import './Checkbox.css';
 import { PropTypes } from 'prop-types';
+import './Checkbox.css';
+import { useState } from 'react';
 
-const Checkbox = ({ children }) => {
+const Checkbox = forwardRef(({ label, children }, ref) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheckboxChange = (event) => {
+    const isChecked = event.target.checked;
+    setChecked(isChecked);
+    // onChange && onChange(isChecked);
+  };
+
   return (
-    <>
-      <FormCheck>
-        <FormCheck.Label className="checkbox-text">
-          <FormCheck.Input className="sag-checkbox" />
-          {children}
-        </FormCheck.Label>
-      </FormCheck>
-    </>
+    <FormCheck>
+      <FormCheck.Label className=".sag-checkbox__label">
+        <FormCheck.Input
+          type="checkbox"
+          label={label}
+          checked={checked}
+          onChange={handleCheckboxChange}
+          ref={ref}
+          className="sag-checkbox"
+        />
+        {children}
+      </FormCheck.Label>
+    </FormCheck>
   );
-};
+});
 
 Checkbox.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
+  label: PropTypes.node,
 };
 
 export default Checkbox;
