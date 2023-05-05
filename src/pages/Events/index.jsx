3142e0ce69ae897165dc3c89f2f0859f3817262b
Krish2958 <krishm.km17@gmail.com>
2023-05-05 14:37:16 +0530

    Added Changes

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -3,29 +3,7 @@ import EventCard from './components/Card';
 import { LogoIdentifier } from '../../components';
 
 export const Events = () => {
-  return (
-    <div className="title">
-      Events
-      <EventCard
-        title="Essay Writing Competition"
-        category={['Competition', 'Quizzes']}
-        clubname="Art & Craft Club"
-        eventdate="23"
-        month="May"
-        pastEvent={false}
-        clublogo={LogoIdentifier.ArtAndCraftClub}
-      />
-      <EventCard
-        title="Semantics"
-        category={['Competition', 'Quizzes']}
-        clubname="Literal Club"
-        eventdate="23"
-        month="Mar"
-        pastEvent={true}
-        clublogo={LogoIdentifier.LiteralClub}
-      />
-    </div>
-  );
+  return <div className="title">Events</div>;
 };
 
 Events.route = '/events';
